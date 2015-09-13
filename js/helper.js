0de931273618fb7890400843b67a703fa4a55981
Adi Li <ctlihk@live.hk>
2015-09-13 17:15:30 +0800

    replaced my own data

--- js/helper.js
+++ js/helper.js
@@ -13,7 +13,7 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
+var HTMLheaderRole = '<span class="role">%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -30,30 +30,30 @@ var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkEmployer = '<a href="#" target="_blank">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectTitle = '<a href="#" target="_blank">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolName = '<a href="#" target="_blank">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineTitle = '<a href="#" target="_blank">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineURL = '<br><a href="#" target="_blank">%data%</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
@@ -86,6 +86,7 @@ function logClicks(x,y) {
 
 $(document).click(function(loc) {
   // your code goes here!
+  logClicks(loc.pageX, loc.pageY);
 });
 
 
@@ -106,7 +107,10 @@ function initializeMap() {
   var locations;
 
   var mapOptions = {
-    disableDefaultUI: true
+    disableDefaultUI: true,
+    panControl: true,
+    zoomControl: true,
+    streetViewControl: true
   };
 
   /* 
@@ -173,6 +177,7 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      marker.setAnimation(google.maps.Animation.DROP);
     });
 
     // this is where the pin actually gets added to the map.
@@ -235,11 +240,18 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+  // Make sure the map bounds get updated on page resize
+  map.fitBounds(mapBounds);
+});
+
+// Animating scrolling to element.
+$(document).on('click', 'nav > a', function(e){
+  e.preventDefault();
+  var target = $(e.target).attr('href');
+  $("html, body").animate({scrollTop: $(target).offset().top}, 500);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,183 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var bio = {
+	"name": "Adi Li",
+	"role": "Web Developer",
+	"contacts": {
+		"mobile": "1234-5678",
+		"email": "ctlihk@live.hk",
+		"github": "adi-li",
+		"twitter": "@ctlihk",
+		"location": "Hong Kong"
+	},
+	"welcomeMessage": "Hello world!",
+	"skills": [
+		"HTML", "CSS", "JavaScript"
+	],
+	"biopic": "https://avatars1.githubusercontent.com/u/1830367?v=3&s=460",
+	display: function() {
+		var headerDOM = $('#header');
+		var topContactsDOM = $('#topContacts');
+		var footerContactsDOM = $('#footerContacts');
+
+		headerDOM.prepend(HTMLheaderRole.replace('%data%', bio.role));
+		headerDOM.prepend(HTMLheaderName.replace('%data%', bio.name));
+
+		for (var contactType in bio.contacts) {
+			var contact = HTMLcontactGeneric.replace(
+				'%contact%', contactType
+			).replace(
+				'%data%', bio.contacts[contactType]
+			);
+			topContactsDOM.append(contact);
+			footerContactsDOM.append(contact);
+		}
+
+		headerDOM.append(HTMLbioPic.replace('%data%', bio.biopic));
+		headerDOM.append(HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage));
+
+		if (bio.skills.length > 0) {
+			headerDOM.append(HTMLskillsStart);
+		}
+		for (var i = 0; i < bio.skills.length; i++) {
+			headerDOM.append(HTMLskills.replace('%data%', bio.skills[i]));
+		}
+	}
+};
+
+var work = {
+	"jobs": [
+		{
+			"employer": "Snaptee Ltd.",
+			"title": "Software Engineer",
+			"location": "Lai Chi Kok, Hong Kong",
+			"dates": "Jan, 2013 - present",
+			"description": "Design your personalized t-shirts with Snaptee. Get it from AppStore and Google Play. Create your t-shirt collection with Instagram photos or your favorite snapshot pictures. Snaptee realize your creativity!",
+			"url": "https://snaptee.co"
+		},
+		{
+			"employer": "GoBuya",
+			"title": "Web 2.0 and Mobile Website Developer",
+			"location": "Kwai Chung, Hong Kong",
+			"dates": "2011 - Jan, 2013",
+			"description": "GoBuya《夠買》團購提供香港最新最Hit團購優惠，包括餐飲團購、美食團購、紅酒團購、戲票團購、優惠券團購、美容團購、化妝品團購、課程團購，團購力量全港最大！",
+			"url": "http://www.gobuya.com"
+		},
+	],
+	display: function() {
+		var workExperienceDOM = $('#workExperience');
+		for (var i = 0; i < work.jobs.length; i++) {
+			var job = work.jobs[i];
+			var entry = $(HTMLworkStart);
+			entry.append(
+				HTMLworkEmployer.replace('%data%', job.employer).replace('#', job.url) + 
+				HTMLworkTitle.replace('%data%', job.title)
+			);
+			entry.append(HTMLworkDates.replace('%data%', job.dates));
+			entry.append(HTMLworkLocation.replace('%data%', job.location));
+			entry.append(HTMLworkDescription.replace('%data%', job.description));
+			workExperienceDOM.append(entry);
+		};
+	}
+};
+
+var education = {
+	"schools": [
+		{
+			"name": "The Chinese University of Hong Kong",
+			"location": "The Chinese University of Hong Kong",
+			"degree": "Bachelor of Engineering",
+			"majors": ["Information Engineering"],
+			"dates": 2013,
+			"url": "http://www.ie.cuhk.edu.hk/main/index.shtml"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Front-End Web Developer Nanodegree",
+			"school": "Udacity",
+			"date": 2015,
+			"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+		},
+		{
+			"title": "iOS Developer Nanodegree",
+			"school": "Udacity",
+			"date": 2015,
+			"url": "https://www.udacity.com/course/ios-developer-nanodegree--nd003"
+		}
+	],
+	display: function() {
+		var educationDOM = $('#education');
+		for (var i = 0; i < education.schools.length; i++) {
+			var school = education.schools[i];
+			var entry = $(HTMLschoolStart);
+			entry.append(
+				HTMLschoolName.replace('%data%', school.name).replace('#', school.url) +
+				HTMLschoolDegree.replace('%data%', school.degree)
+			);
+			entry.append(HTMLschoolDates.replace('%data%', school.dates));
+			entry.append(HTMLschoolLocation.replace('%data%', school.location));
+			entry.append(HTMLschoolMajor.replace('%data%', school.majors.join(',')));
+			educationDOM.append(entry);
+		}
+
+		if (education.onlineCourses.length > 0) {
+			educationDOM.append(HTMLonlineClasses);
+		}
+
+		for (var i = 0; i < education.onlineCourses.length; i++) {
+			var course = education.onlineCourses[i];
+			var entry = $(HTMLschoolStart);
+			entry.append(
+				HTMLonlineTitle.replace('%data%', course.title).replace('#', course.url) +
+				HTMLonlineSchool.replace('%data%', course.school)
+			);
+			entry.append(HTMLonlineDates.replace('%data%', course.date))
+			entry.append(
+				HTMLonlineURL.replace('%data%', course.url).replace('#', course.url)
+			)
+			educationDOM.append(entry);
+		}
+	}
+};
+
+var projects = {
+	"projects": [
+		{
+			"title": "Pitch Perfect",
+			"dates": "2015 - 2015",
+			"description": "iOS Developer Nanodegree - Project 1",
+			"images": [
+				"./images/project-1-1.png",
+				"./images/project-1-2.png"
+			]
+		}
+	],
+	display: function() {
+		var projectsDOM = $('#projects');
+		for (var i = 0; i < projects.projects.length; i++) {
+			var project = projects.projects[i];
+			var entry = $(HTMLprojectStart);
+			entry.append(
+				HTMLprojectTitle.replace('%data%', project.title)
+			);
+			entry.append(HTMLprojectDates.replace('%data%', project.dates));
+			entry.append(HTMLprojectDescription.replace('%data%', project.description));
+			for (var j = 0; j < project.images.length; j++) {
+				entry.append(HTMLprojectImage.replace('%data%', project.images[j]));
+			}
+			projectsDOM.append(entry);
+		}
+	}
+}
+
+$(function(){
+	bio.display();
+	work.display();
+	education.display();
+	projects.display();
+
+	$('#mapDiv').append(googleMap);
+});
